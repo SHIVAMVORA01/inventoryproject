d236246faed6f9360c58202e3053ffbf8097b24a
SHIVAMVORA01 <67798427+SHIVAMVORA01@users.noreply.github.com>
2021-04-30 16:32:54 +0530

    project

---
+++ dashboard/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product, Order
+
+admin.site.site_header = 'Dashboard'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','category', 'quantity')
+    list_filter = ['category']
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
